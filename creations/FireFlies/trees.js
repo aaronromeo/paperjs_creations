1f58b5af22b3003258dddb619c2e381b831d338d
Aaron Romeo <aaron@aaronromeo.com>
2021-08-07 21:41:20 -0400

    Multiple trees and a new background for the sky

--- creations/FireFlies/trees.js
+++ creations/FireFlies/trees.js
@@ -1,4 +1,5 @@
 FIELD_COLOUR = '#666666';
+TREE_TRUNK_COLOUR = '#444444';
 CANVAS_WIDTH = document.querySelector('#starsCanvas').clientWidth;
 CANVAS_HEIGHT = document.querySelector('#starsCanvas').clientHeight;
 TERRA_CURVE_HEIGHT = CANVAS_WIDTH / 28.8;
@@ -7,28 +8,33 @@ GRASS_LINE = CANVAS_HEIGHT * 0.5;
 TREE_HEIGHT = CANVAS_HEIGHT * 0.20;
 
 class TreeLine {
-  drawTree() {
-    console.log(TREE_LINE, GRASS_LINE);
-    // const p01seg01point = new Point(150, 200);
-    // const p01seg02point = new Point(200, 700);
+  drawTree(pointX) {
+    // console.log(TREE_LINE, GRASS_LINE);
+    // const p01TopPoint = new Point(150, 200);
+    // const p01BotPoint = new Point(200, 700);
     // const p01seg01hOunt = new Point(150, 150);
-    console.log(TREE_HEIGHT, TREE_LINE, TREE_LINE - TREE_HEIGHT);
-    const p01seg01point = new Point(150, TREE_HEIGHT);
-    const p01seg02point = new Point(150 + (GRASS_LINE - TREE_HEIGHT) / 10, GRASS_LINE);
-    const p01seg01hOunt = new Point(150, 150);
-    const p01seg01hIn = new Point(0, 0);
-    const p01r1seg = new Segment(p01seg01point, null, p01seg01hOunt);
-    const p01r2seg = new Segment(p01seg02point, p01seg01hIn, null);
+    // console.log(TREE_HEIGHT, TREE_LINE, TREE_LINE - TREE_HEIGHT);
+    const treeHeight = TREE_HEIGHT + ((TREE_HEIGHT / 2.0) * Math.random());
+    const treeBottom = GRASS_LINE + 20;
+    const segmentCount = [2, 3, 5, 3][Math.round(Math.random() * 3)];
+    console.log(treeHeight, segmentCount);
 
+    const p01TopX = pointX;
+    const p01BotX = pointX + (treeBottom - treeHeight) / 10;
+    const p01TopPoint = new Point(p01TopX, treeHeight);
+    const p01BotPoint = new Point(p01BotX, treeBottom);
+    const p01seg01hOunt = new Point(treeHeight / segmentCount, treeHeight / segmentCount);
+    const p01seg01hIn = new Point(0, 0);
+    const p01r1seg = new Segment(p01TopPoint, null, p01seg01hOunt);
+    const p01r2seg = new Segment(p01BotPoint, p01seg01hIn, null);
     const p01 = new Path(p01r1seg, p01r2seg);
 
-    const p02seg01point = p01.getPointAt(p01.length / 3);
-    const p02seg02point = p01.getPointAt(p01.length / 3) + new Point(100, -50);
-    const p02seg01hOunt = new Point(25, -25);
-    const p02seg01hIn = new Point(-25, -25);
-    const p02r1seg = new Segment(p02seg01point, null, p02seg01hOunt);
-    const p02r2seg = new Segment(p02seg02point, p02seg01hIn, null);
-
+    const p02TopPoint = p01.getPointAt(p01.length / segmentCount);
+    const p02BotPoint = p01.getPointAt(p01.length / segmentCount) + new Point((p01BotX - p01TopX) * segmentCount, -(p01BotX - p01TopX) * (segmentCount / 2));
+    const p02seg01hOunt = new Point(p01.length / (segmentCount * 3), -(p01.length / (segmentCount * 3)));
+    const p02seg01hIn = new Point(-(p01.length / (segmentCount * 3)), -(p01.length / (segmentCount * 3)));
+    const p02r1seg = new Segment(p02TopPoint, null, p02seg01hOunt);
+    const p02r2seg = new Segment(p02BotPoint, p02seg01hIn, null);
     const p02 = new Path(p02r1seg, p02r2seg);
 
     const path = new CompoundPath({
@@ -38,54 +44,63 @@ class TreeLine {
       ],
     });
 
-    path.strokeColor = FIELD_COLOUR;
-    path.strokeWidth = 10;
+    path.strokeColor = TREE_TRUNK_COLOUR;
+    path.strokeWidth = 4 * Math.random() + 6;
 
-    let l01 = p01seg01point.clone();
-    const t01bounds = new Path.Circle(l01, 100)
-    for (let i = 0; i < 500; i++) {
+    let l01 = p01TopPoint.clone();
+    const t01bounds = new Path.Circle(l01, p01.length / 3)
+    for (let i = 0; i < p01.length * 1.5; i++) {
       let c01 = new Path.Circle(l01, 10);
       c01.strokeColor = FIELD_COLOUR;
       if (Math.random() > 0.6) {
         c01.fillColor = 'black';
         c01.opacity = 0.5;
       }
-      l01 = p01seg01point.clone() + new Point(
+      l01 = p01TopPoint.clone() + new Point(
         (Math.random() * 300.0) - 150.0,
         (Math.random() * 300.0) - 150.0
       );
       while (!t01bounds.contains(l01)) {
-        l01 = p01seg01point.clone() + new Point(
+        l01 = p01TopPoint.clone() + new Point(
           (Math.random() * 300.0) - 150.0,
           (Math.random() * 300.0) - 150.0
         );
       }
     }
 
-    let l02 = p02seg02point.clone();
-    const t02bounds = new Path.Circle(l02, 75);
-    for (let i = 0; i < 300; i++) {
+    let l02 = p02BotPoint.clone();
+    const t02bounds = new Path.Circle(l02, p02.length / 3);
+    for (let i = 0; i < p02.length * 1.5; i++) {
       let c02 = new Path.Circle(l02, 10);
       c02.strokeColor = FIELD_COLOUR;
       if (Math.random() > 0.6) {
         c02.fillColor = 'black';
         c02.opacity = 0.5;
       }
-      l02 = p02seg02point.clone() + new Point(
+      l02 = p02BotPoint.clone() + new Point(
         (Math.random() * 200.0) - 100.0,
         (Math.random() * 200.0) - 100.0
       );
       while (!t02bounds.contains(l02)) {
-        l02 = p02seg02point.clone() + new Point(
+        l02 = p02BotPoint.clone() + new Point(
           (Math.random() * 200.0) - 100.0,
           (Math.random() * 200.0) - 100.0
         );
       }
     }
+
+    path.bringToFront();
   }
 
   draw() {
-    this.drawTree();
+    let currentPosition = 50;
+    const treePositions = [];
+    while (currentPosition < CANVAS_WIDTH) {
+      treePositions.push(currentPosition);
+      currentPosition = currentPosition + (20 + 80 * Math.random());
+    }
+
+    treePositions.sort(() => Math.random() - 0.5).forEach(pos => this.drawTree(pos));
   }
 }
 
@@ -143,11 +158,11 @@ class Field {
 }
 
 const generateTerra = () => {
+  const field = new Field();
+  field.draw();
+
   const treeLine = new TreeLine();
   treeLine.draw();
-
-  const field = new Field();
-  // field.draw();
 }
 
 generateTerra();
